28b6a1c834cf160c002e5333162e7e2a6ce0eb2b
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-06-30 14:02:01 +0700

    feat: add chat service for managing logic

---
+++ internal/service/chat.go
@@ -0,0 +1,101 @@
+package service
+
+import (
+	"google-login/entity"
+	"google-login/internal/repository"
+	"google-login/model"
+	"google-login/pkg/database/mariadb"
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type IChatService interface {
+	GetMessagesByConversationID(convoID uuid.UUID) ([]*entity.Message, error)
+	SendMessage(param model.SendMessageInput) error
+	CreateConversation(param model.CreateConversationInput) error
+	GetConversationsByUser(userID int) ([]*entity.Conversation, error)
+}
+
+type ChatService struct {
+	db             *gorm.DB
+	ChatRepository repository.IChatRepository
+}
+
+func NewChatService(chatRepository repository.IChatRepository) IChatService {
+	return &ChatService{
+		db:             mariadb.Connection,
+		ChatRepository: chatRepository,
+	}
+}
+
+func (s *ChatService) CreateConversation(param model.CreateConversationInput) error {
+	tx := s.db.Begin()
+	defer tx.Rollback()
+
+	convo := &entity.Conversation{
+		ConversationID: uuid.New(),
+		Title:          param.Title,
+		IsGroup:        len(param.UserIDs) > 2,
+		CreatedAt:      time.Now(),
+	}
+
+	err := s.ChatRepository.CreateConversation(tx, convo)
+	if err != nil {
+		return err
+	}
+
+	for _, userID := range param.UserIDs {
+		participant := &entity.Participant{
+			ParticipantID:  uuid.New(),
+			ConversationID: convo.ConversationID,
+			UserID:         userID,
+			JoinedAt:       time.Now(),
+		}
+		err := s.ChatRepository.CreateParticipant(tx, participant)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *ChatService) SendMessage(param model.SendMessageInput) error {
+	tx := s.db.Begin()
+	defer tx.Rollback()
+
+	msg := &entity.Message{
+		MessageID:      uuid.New(),
+		ConversationID: param.ConversationID,
+		UserID:         param.UserID,
+		Content:        param.Content,
+		CreatedAt:      time.Now(),
+	}
+
+	err := s.ChatRepository.CreateMessage(tx, msg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *ChatService) GetMessagesByConversationID(convoID uuid.UUID) ([]*entity.Message, error) {
+	convos, err := s.ChatRepository.GetMessagesByConversationID(convoID)
+	if err != nil {
+		return nil, err
+	}
+
+	return convos, nil
+}
+
+func (s *ChatService) GetConversationsByUser(userID int) ([]*entity.Conversation, error) {
+	convos, err := s.ChatRepository.GetConversationsByUser(userID)
+	if err != nil {
+		return nil, err
+	}
+
+	return convos, nil
+}
