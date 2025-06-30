54c709074f7cac515816188f1352ea5e3e299a1d
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-06-30 14:01:30 +0700

    feat: add chat repository dependencies

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -4,10 +4,12 @@ import "gorm.io/gorm"
 
 type Repository struct {
 	UserRepository IUserRepository
+	ChatRepository IChatRepository
 }
 
 func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{
 		UserRepository: NewUserRepository(db),
+		ChatRepository: NewChatRepository(db),
 	}
 }
