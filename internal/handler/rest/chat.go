0ce4da8efa34bf11bd87f1d8dad56e5c2fd6f639
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-06-30 14:03:49 +0700

    feat: add handler for chat

---
+++ internal/handler/rest/chat.go
@@ -0,0 +1,80 @@
+package rest
+
+import (
+	"google-login/entity"
+	"google-login/model"
+	"google-login/pkg/response"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+func (r *Rest) SendMessage(c *gin.Context) {
+	var param model.SendMessageInput
+	err := c.ShouldBindJSON(&param)
+	if err != nil {
+		response.Error(c, http.StatusBadRequest, "invalid input", err)
+		return
+	}
+
+	user := c.MustGet("user").(*entity.User)
+	param.UserID = user.UserID
+
+	err = r.service.ChatService.SendMessage(param)
+	if err != nil {
+		response.Error(c, http.StatusInternalServerError, "failed to send message", err)
+		return
+	}
+
+	response.Success(c, http.StatusOK, "success", nil)
+}
+
+func (r *Rest) CreateConversation(c *gin.Context) {
+	var param model.CreateConversationInput
+	err := c.ShouldBindJSON(&param)
+	if err != nil {
+		response.Error(c, http.StatusBadRequest, "invalid input", err)
+		return
+	}
+
+	user := c.MustGet("user").(*entity.User)
+	param.UserIDs = append(param.UserIDs, user.UserID)
+
+	err = r.service.ChatService.CreateConversation(param)
+	if err != nil {
+		response.Error(c, http.StatusInternalServerError, "failed to create conversation", err)
+		return
+	}
+
+	response.Success(c, http.StatusCreated, "success to create conversation", nil)
+}
+
+func (r *Rest) GetMessagesByConversationID(c *gin.Context) {
+	idStr := c.Param("convoID")
+	convoID, err := uuid.Parse(idStr)
+	if err != nil {
+		response.Error(c, http.StatusBadRequest, "invalid conversation id", err)
+		return
+	}
+
+	messages, err := r.service.ChatService.GetMessagesByConversationID(convoID)
+	if err != nil {
+		response.Error(c, http.StatusInternalServerError, "failed to get conversation", err)
+		return
+	}
+
+	response.Success(c, http.StatusOK, "success to get conversation", messages)
+}
+
+func (r *Rest) GetUserConversations(c *gin.Context) {
+	user := c.MustGet("user").(*entity.User)
+
+	convos, err := r.service.ChatService.GetConversationsByUser(user.UserID)
+	if err != nil {
+		response.Error(c, http.StatusInternalServerError, "failed to get conversations", err)
+		return
+	}
+
+	response.Success(c, http.StatusOK, "success to get conversations", convos)
+}
