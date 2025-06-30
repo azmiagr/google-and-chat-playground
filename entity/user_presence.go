9bdde163becbe231a09a92b2e9dc59ca90cb7ab6
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-06-30 14:07:38 +0700

    refactor: changing the name of user presence attributes

--- entity/user_presence.go
+++ entity/user_presence.go
@@ -4,7 +4,7 @@ import "time"
 
 type UserPresence struct {
 	UserID   int       `json:"user_id" gorm:"type:int;primaryKey"`
-	IsBool   bool      `json:"is_bool" gorm:"type:bool"`
+	IsOnline bool      `json:"is_online" gorm:"type:bool"`
 	LastSeen time.Time `json:"last_seen" gorm:"type:timestamp"`
 
 	User User `json:"user"`
