98d0c0b5989d7a6e52618decfb2d404557c8756d
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-06-30 17:24:55 +0700

    feat: add new struct for get message and get conversation response

--- model/chat.go
+++ model/chat.go
@@ -1,5 +1,11 @@
 package model
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type SendMessageInput struct {
 	UserID  int    `json:"user_id"`
 	Content string `json:"content"`
@@ -8,4 +14,19 @@ type SendMessageInput struct {
 type CreateConversationInput struct {
 	UserIDs []int  `json:"user_ids"`
 	Title   string `json:"title"`
+}
+
+type GetMessageResponse struct {
+	ConversationTitle string    `json:"title"`
+	MessageID         uuid.UUID `json:"message_id"`
+	SenderID          int       `json:"sender_id"`
+	Content           string    `json:"content"`
+	CreatedAt         time.Time `json:"created_at"`
+}
+
+type GetConversationResponse struct {
+	ConversationID uuid.UUID `json:"conversation_id"`
+	Title          string    `json:"title"`
+	IsGroup        bool      `json:"is_group"`
+	CreatedAt      time.Time `json:"created_at"`
 }
